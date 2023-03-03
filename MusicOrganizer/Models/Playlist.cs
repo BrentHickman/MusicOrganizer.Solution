625d46cea68a0378bd1971c322d8dc69624ae327
BrentHickman <BrentHickman@ymail.com>
2023-03-02 16:01:18 -0800

    playlist id is nullable and based on list count
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- MusicOrganizer/Models/Playlist.cs
+++ MusicOrganizer/Models/Playlist.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using System;
 
 namespace MusicOrganizer.Models
 {
   public class Playlist
   {
+    #nullable enable
     private static List<Playlist> _instances = new List<Playlist> { };
     private static int _totalCount { get; set; } = 0;
     public string Name { get; set; }
@@ -25,9 +27,16 @@ namespace MusicOrganizer.Models
     {
       _instances.Clear();
     }
-    public static Playlist Find(int searchId)
+    public static Playlist? Find(int searchId)
     {
-      return _instances[searchId-1];
+      foreach (Playlist playlist in _instances)
+      {
+        if (playlist.Id == searchId)
+        {
+          return playlist;
+        }
+      }
+      return null;
     }
     public void AddMedia(Media media)
     {
